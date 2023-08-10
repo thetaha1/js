a8bfb07c2dcc8110a7e1c687fe8b0efb0bf5506a
thetaha1 <millionertaha@gmail.com>
2023-08-11 02:51:21 +0300

    Assignment 4 Challenge

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log(Math.floor(Math.random() * 5)); // 0 || 1 || 2 || 3 || 4
+/*
+  String Challenge
+  All Solutions Must Be In One Chain
+  You Can Use Concatenate
+*/
+
+let a = "Elzero Web School";
+
+// Include This Method In Your Solution [slice, charAt]
+console.log(a.charAt(2).toUpperCase() + a.slice(3, 6)); // Zero
+
+// 8 H
+console.log(a.charAt(13).toUpperCase().repeat(8)); // HHHHHHHH
+
+// Return Array
+console.log(a.split(" ", 1)); // ["Elzero"]
+
+// Use Only "substr" Method + Template Literals In Your Solution
+console.log(`${a.substr(0, 6)} ${a.substr(-6)}`); // Elzero School
+
+// Solution Must Be Dynamic Because String May Changes
+console.log(a.charAt(0).toLowerCase() + a.split(1)); // eLZERO WEB SCHOOl
