cbcf57d412cab95c29cb599f45bf161bb41d5b39
thetaha1 <millionertaha@gmail.com>
2023-08-10 03:28:17 +0300

    Assignment 2 3

--- app.js
+++ app.js
@@ -1,13 +1,3 @@
-// Add Variables Here
-var numberOne = 10;
-var numberTwo = 20;
-// Ouput
-console.log(numberOne + "" + numberTwo); // Normal Concatenate => 1020
-console.log(typeof (numberOne + "" + numberTwo)); // Normal Concatenate => String
-console.log(typeof `${numberOne}${numberTwo}`); // Template Literals Way => 1020
-console.log(`${numberOne}${numberTwo}`); // Template Literals Way => String
-
-console.log(numberTwo + "\n" + numberOne);
-
-console.log(`${numberTwo}
-${numberOne}`);
+console.log(
+  '`I\'m In\n\\\\\nLove \\\\ """ \'\'\'\n++ With ++\n\\"""\\"""\n""JavaScript""``'
+);
