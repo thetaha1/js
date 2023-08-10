d2ad893ae69285a1e4640683733ea7c2eeee4b60
thetaha1 <millionertaha@gmail.com>
2023-08-10 20:29:08 +0300

    assignment 3 1

--- app.js
+++ app.js
@@ -1,24 +1,17 @@
-/*
-  Number Challenge
-*/
+// Examples
+console.log(100_000); // 100000
+console.log(100000); // 100000
+console.log(5e4 + 5e4); // 100000
 
-let a = 100;
-let b = 2_00.5;
-let c = 1e2;
-let d = 2.4;
-
-// Find Smallest Number In All Variables And Return Integer
-console.log(Math.trunc(Math.min(a, b, c, d)));
-
-// Use Variables a + d One Time To Get The Needed Output
-console.log(Math.pow(a, Math.trunc(d))); // 10000
-
-// Get Integer "2" From d Variable With 4 Methods
-console.log(Number.parseInt(d));
-console.log(Math.trunc(d));
-console.log(Math.floor(d));
-console.log(Math.round(d));
-
-// Use Variables b + d To Get This Values
-console.log((Math.trunc(b) / Math.ceil(d)).toFixed(2)); // 66.67 => String
-console.log(Number((Math.trunc(b) / Math.ceil(d)).toFixed(2))); // 67 => Number
+// Your Solutions
+console.log(100000); // 100000
+console.log(100_000); // 100000
+console.log(1e5); // 100000
+console.log(5e4 + 5e4); // 100000
+console.log(Number(100000)); // 100000
+console.log(Math.round(100000)); // 100000
+console.log(Math.ceil(100000)); // 100000
+console.log(Math.floor(100000)); // 100000
+console.log(Math.trunc(100000)); // 100000
+console.log(Math.min(100000)); // 100000
+console.log(Math.max(100000)); // 100000
