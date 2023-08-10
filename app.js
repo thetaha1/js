78523a564b928d21335568d9fa98cb9dca0a9fae
thetaha1 <millionertaha@gmail.com>
2023-08-10 21:48:54 +0300

    Ass 3.6

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-let num = 10;
+let flt = 10.4;
 
-let status = Number.isInteger(num);
-
-console.log(++status); // 2
+console.log(Number.parseInt(flt)); // 10
+console.log(Math.trunc(Number.parseFloat(flt))); // 10
+console.log(Math.trunc(flt)); // 10
+console.log(Math.floor(flt)); // 10
+console.log(Math.round(flt)); // 10
