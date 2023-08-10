6dbe961abd4ae492fc3f25388470106f73836de4
thetaha1 <millionertaha@gmail.com>
2023-08-11 02:57:25 +0300

    Ass 5.1

--- app.js
+++ app.js
@@ -1,22 +1,7 @@
-/*
-  String Challenge
-  All Solutions Must Be In One Chain
-  You Can Use Concatenate
-*/
-
-let a = "Elzero Web School";
-
-// Include This Method In Your Solution [slice, charAt]
-console.log(a.charAt(2).toUpperCase() + a.slice(3, 6)); // Zero
-
-// 8 H
-console.log(a.charAt(13).toUpperCase().repeat(8)); // HHHHHHHH
-
-// Return Array
-console.log(a.split(" ", 1)); // ["Elzero"]
-
-// Use Only "substr" Method + Template Literals In Your Solution
-console.log(`${a.substr(0, 6)} ${a.substr(-6)}`); // Elzero School
-
-// Solution Must Be Dynamic Because String May Changes
-console.log(a.charAt(0).toLowerCase() + a.split(1)); // eLZERO WEB SCHOOl
+let userName = "Elzero";
+console.log(userName.charAt(0).toLowerCase()); // e
+console.log(userName[0].toLowerCase()); // e
+console.log(userName.slice(0, 1).toLowerCase()); // e
+console.log(userName.substring(0, 1).toLowerCase()); // e
+console.log(userName.substr(0, 1).toLowerCase()); // e
+console.log(userName.split("", 1).toString().toLowerCase().repeat(3)); // eee
