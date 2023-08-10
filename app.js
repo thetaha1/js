e56daf1ca834568a15b4ec3df24a0c4bab5354a0
thetaha1 <millionertaha@gmail.com>
2023-08-10 06:42:17 +0300

    during

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
-console.log(
-  '`I\'m In\n\\\\\nLove \\\\ """ \'\'\'\n++ With ++\n\\"""\\"""\n""JavaScript""``'
-);
+/*
+  Challenge 1
+*/
+
+let a = 10;
+let b = "20";
+let c = 80;
+
+console.log(++a + +b++ + +c++ - +a++);                     //100
+console.log(++a + -b + +c++ - -a++ + +a);                  //100
+console.log(--c + +b + --a * +b++ - +b * a + --a - +true); //100
+
+/*
+  [++a] [+]
+  [++a]
+  - Value:
+  - Explain:
+  [+]
+  - Explain:
+*/
+
+/*
+  Challenge 2
+*/
+
+let d = "-100";
+let e = "20";
+let f = 30;
+let g = true;
+
+// Only Use Variables Value
+// Do Not Use Variable Twice
+
+console.log(); // 2000
+console.log(); // 173
