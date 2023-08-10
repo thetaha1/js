8b8131440a004a7404744f8502222fec4affd054
thetaha1 <millionertaha@gmail.com>
2023-08-10 20:30:02 +0300

    Ass 3 2

--- app.js
+++ app.js
@@ -1,17 +1,1 @@
-// Examples
-console.log(100_000); // 100000
-console.log(100000); // 100000
-console.log(5e4 + 5e4); // 100000
-
-// Your Solutions
-console.log(100000); // 100000
-console.log(100_000); // 100000
-console.log(1e5); // 100000
-console.log(5e4 + 5e4); // 100000
-console.log(Number(100000)); // 100000
-console.log(Math.round(100000)); // 100000
-console.log(Math.ceil(100000)); // 100000
-console.log(Math.floor(100000)); // 100000
-console.log(Math.trunc(100000)); // 100000
-console.log(Math.min(100000)); // 100000
-console.log(Math.max(100000)); // 100000
+console.log(Number.MAX_SAFE_INTEGER); // 9007199254740991
