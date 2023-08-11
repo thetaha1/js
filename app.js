a77b6cfc1202a5a323bb3b52491a4a604b927675
thetaha1 <millionertaha@gmail.com>
2023-08-11 14:12:00 +0300

    Ass 7.1

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-let num = 8;
+let num1 = 9;
+let str = "9";
+let str2 = "20";
 
-// In Test Case 1
-if (num < 10) console.log(`00${num}`);
-else if (num >= 10 && num < 100) console.log(`0${num}`);
-else if (num >= 100) console.log(num);
+// Output
+if (num1 == str) console.log("Done");
+("{num1} Is The Same Value As {str}");
+
+("{num1} Is The Same Value As {str} But Not The Same Type");
+if (num1 === str) console.log("Done");
+
+("{num1} Is Not The Same Value Or The Same Type As {str2}");
+if (num1 != str) console.log("Done");
+
+("{str} Is The Same Type As {str2} But Not The Same Value");
+if (typeof str != typeof str && str != str2) console.log("Done");
