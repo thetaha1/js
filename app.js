93ab90e5a181f54c7b484f5794c2937986578f14
thetaha1 <millionertaha@gmail.com>
2023-08-11 03:34:21 +0300

    Ass 6.2

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-console.log(100 == "100"); // true
-console.log(100 < 1000); // true
-console.log(110 + 100 + 10 > 20); // true
-console.log(-10 == "-10"); // true
-console.log(!( -50 >= +"-40")); // true
-console.log(!( 10 >= -"-40")); // true
-console.log(!( "10" != 10)); // true
-console.log(!( 20 * false)); // true
+let num1 = 10;
+let num2 = 20;
+
+console.log(num1 < num2); // true
+console.log(num1 <= num2); // true
+console.log(!(num1 > num2)); // true
+console.log(!(num1 >= num2)); // true
+console.log(num1 != num2); // true
+console.log(num1 !== num2); // true
+console.log(typeof num1 == typeof num2); // true
