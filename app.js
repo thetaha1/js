cce7d6e58d0acdf9abc469d048226cd3a52c6279
thetaha1 <millionertaha@gmail.com>
2023-08-11 15:18:27 +0300

    Ass 7 last

--- app.js
+++ app.js
@@ -1,18 +1,68 @@
-let num1 = 10;
-let num2 = 30;
-let num3 = "30";
+// Edit What You Want Here
 
-// Needed Output
-if (num3 > num1 && typeof num3 != num1)
-  console.log(
-    "30 Is Larger Than 10 And Type string Not The Same Type As number"
-  );
-if (num3 > num1 && num3 == num2 && typeof num3 !== typeof num2)
-  console.log(
-    "30 Is Larger Than 10 And Value Is The Same As 30 And Type string Not The Same Type As number"
-  );
+let num1 = 2;
+let num2 = 1;
+let num3 = 2;
+let num4 = 24;
 
-if (num3 !== num1 && typeof num3 != typeof num2)
-  console.log(
-    "{num3} Value And Type Is Not The Same As {num1} And Type Is Not The Same As {num2}"
-  );
+/*
+  Do Not Edit Below This Line
+  Needed Output
+  True 7 Times
+*/
+
+// Condition 1
+
+if (num1 > num2) {
+  console.log("True");
+} else {
+  console.log("False");
+}
+
+// Condition 2
+
+if (num1 > num2 && num1 < num4) {
+  console.log("True");
+} else {
+  console.log("False");
+}
+
+// Condition 3
+
+if (num1 > num2 && num1 === num3) {
+  console.log("True");
+} else {
+  console.log("False");
+}
+
+// Condition 4
+
+if (num1 + num2 < num4) {
+  console.log("True");
+} else {
+  console.log("False");
+}
+
+// Condition 5
+
+if (num1 + num3 < num4) {
+  console.log("True");
+} else {
+  console.log("False");
+}
+
+// Condition 6
+
+if (num1 + num2 + num3 < num4) {
+  console.log("True");
+} else {
+  console.log("False");
+}
+
+// Condition 7
+// if (24 - (2 + 2) + 1 === 21)
+if (num4 - (num1 + num3) + num2 === 21) {
+  console.log("True");
+} else {
+  console.log("False");
+}
