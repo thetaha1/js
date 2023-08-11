4de6930995853d1bf0c6ab30d8ab6f00dc606193
thetaha1 <millionertaha@gmail.com>
2023-08-11 23:19:21 +0300

    Ass 8.1

--- app.js
+++ app.js
@@ -1,52 +1,22 @@
-/*
-  Switch Challenge
-*/
+let day = "  friday  ";
+// You Need To Remove Spaces And Make First Letter Capital => Friday
+day = `${day.trim().charAt(0).toUpperCase()}${day.trim().substr(1)}`;
 
-let job = "Manager";
-let salary = 0;
-
-switch (job) {
-  case "Manager":
-    salary = 8000;
-    console.log(`My Money is ${salary}`);
+switch (day) {
+  case "Friday":
+  case "Saturday":
+  case "Sunday":
+    console.log("No Appointments Available");
     break;
-  case "IT":
-  case "Support":
-    salary = 6000;
-    console.log(`My Money is ${salary}`);
+  case "Monday":
+    console.log("From 10:00 AM To 5:00 PM");
     break;
-  case "Developer":
-  case "Designer":
-    salary = 7000;
-    console.log(`My Money is ${salary}`);
+  case "Tuesday":
+    console.log("From 10:00 AM To 6:00 PM");
+    break;
+  case "Wednesday":
+    console.log("From 10:00 AM To 7:00 PM");
     break;
   default:
-    salary = 4000;
-}
-
-/*
-  If Challenge
-*/
-
-let holidays = 0;
-let money = 0;
-
-if (holidays === 0) {
-  money = 5000;
-  console.log(`My Money is ${money}`);
-} else if (holidays === 1 || holidays === 2) {
-  money = 3000;
-  console.log(`My Money is ${money}`);
-} else if (holidays === 3) {
-  money = 2000;
-  console.log(`My Money is ${money}`);
-} else if (holidays === 4) {
-  money = 1000;
-  console.log(`My Money is ${money}`);
-} else if (holidays === 5) {
-  money = 0;
-  console.log(`My Money is ${money}`);
-} else {
-  money = 0;
-  console.log(`My Money is ${money}`);
+    console.log("Its Not A Valid Day");
 }
