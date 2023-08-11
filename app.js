ab5518c4fc9f42f2f9d08e861a71a72f21c73980
thetaha1 <millionertaha@gmail.com>
2023-08-11 06:42:11 +0300

    Assignment 7 Challenge

--- app.js
+++ app.js
@@ -1,8 +1,49 @@
-let a = 20;
-let b = 30;
-let c = 10;
+/*
+  If Condition Challenge
+*/
 
-console.log((a < b && a === c) || a < b); // true
-console.log(a + b - a > c); // true
-// console.log(!(false && false));
-console.log(!(a > b) && !(a > b) && !(a < c) && !(a < c)); // true
+let a = 10;
+
+if (a < 10) {
+  console.log(10);
+} else if (a >= 10 && a <= 40) {
+  console.log("10 To 40");
+} else if (a > 40) {
+  console.log("> 40");
+} else {
+  console.log("Unknown");
+}
+
+// Write Previous Condition With Ternary If Syntax
+a < 10
+  ? console.log(10)
+  : a >= 10 && a <= 40
+  ? console.log("10 To 40")
+  : a > 40
+  ? console.log("> 40")
+  : console.log("Unknown");
+
+let st = "Elzero Web School";
+
+// W Position May Change
+if (st.length * 2 === 34) {
+  console.log("Good");
+}
+
+if (st.substr(st.indexOf("W"), 1).toLowerCase() === "w") {
+  console.log("Good");
+}
+
+if (typeof st.length !== "string") {
+  console.log("Good");
+}
+
+if (typeof st.length === "number") {
+  console.log("Good");
+}
+
+console.log();
+
+if (st.split(" ")[0].repeat(2) === "ElzeroElzero") {
+  console.log("Good");
+}
