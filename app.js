a603be7243bdb0f3ee7f9b6c13313ddc8332ac6b
thetaha1 <millionertaha@gmail.com>
2023-08-11 06:49:26 +0300

    Ass 7.1

--- app.js
+++ app.js
@@ -1,49 +1,6 @@
-/*
-  If Condition Challenge
-*/
+let num = 8;
 
-let a = 10;
-
-if (a < 10) {
-  console.log(10);
-} else if (a >= 10 && a <= 40) {
-  console.log("10 To 40");
-} else if (a > 40) {
-  console.log("> 40");
-} else {
-  console.log("Unknown");
-}
-
-// Write Previous Condition With Ternary If Syntax
-a < 10
-  ? console.log(10)
-  : a >= 10 && a <= 40
-  ? console.log("10 To 40")
-  : a > 40
-  ? console.log("> 40")
-  : console.log("Unknown");
-
-let st = "Elzero Web School";
-
-// W Position May Change
-if (st.length * 2 === 34) {
-  console.log("Good");
-}
-
-if (st.substr(st.indexOf("W"), 1).toLowerCase() === "w") {
-  console.log("Good");
-}
-
-if (typeof st.length !== "string") {
-  console.log("Good");
-}
-
-if (typeof st.length === "number") {
-  console.log("Good");
-}
-
-console.log();
-
-if (st.split(" ")[0].repeat(2) === "ElzeroElzero") {
-  console.log("Good");
-}
+// In Test Case 1
+if (num < 10) console.log(`00${num}`);
+else if (num >= 10 && num < 100) console.log(`0${num}`);
+else if (num >= 100) console.log(num);
