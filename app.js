82b2955c6eae9ca32b9da71236c33edd57072838
thetaha1 <millionertaha@gmail.com>
2023-08-12 06:24:13 +0300

    Ass 9.5 Updated

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-let website = "Go";
-let words = [`${website}ogle`, "Facebook", ["Elzero", "Web", "School"]];
+let needle = "JS";
+let haystack = ["PHP", "JS", "Python"];
 
-console.log(words[website.length][+false].slice(website.length).toUpperCase()); // ZERO
-console.log(words[website.length][website.length - website.length].slice(website.length).toUpperCase()); // ZERO
+if (haystack.includes(needle)) {
+    console.log("Found");
+}
+
+if (haystack.indexOf(needle) != -true) {
+    console.log("Found");
+}
+if (haystack[+true] === needle) {
+    console.log("Found");
+}
+if (haystack.indexOf(needle)) {
+    console.log("Found");
+}
