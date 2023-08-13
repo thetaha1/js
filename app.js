1496d861ff88786209e5aa5c20596d0dcfc89fe5
thetaha1 <millionertaha@gmail.com>
2023-08-13 07:38:21 +0300

    Ass 10.2

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 let start = 10;
-let end = 100;
-let exclude = 40;
+let end = 0;
+let stop = 3;
 
-for (let i = start;i <= end; i+=start ){
-  if (i === exclude) {
-    continue;
+for (let i = start; i > end; i--) {
+  if (i < start) {
+    console.log(`${end}${i}`);
+    if (i === stop) break;
+  } else {
+    console.log(i);
   }
-  console.log(i);
 }
