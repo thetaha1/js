6131e2c0d317015c4b75def1909a76405e70b483
thetaha1 <millionertaha@gmail.com>
2023-08-13 18:10:42 +0300

    Ass 11 Challenge

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
-let start = 0;
-let mix = [1, 2, 3, "A", "B", "C", 4];
+// Loop Challenge
 
-for (let i = start + 1; i < mix.length; ++i) {
-  if (typeof mix[i] === "number") {
-    console.log(mix[i]);
+let myAdmins = ["Ahmed", "Osama", "Sayed", "Stop", "Samera"];
+let myEmployees = [
+  "Amgad",
+  "Samah",
+  "Ameer",
+  "Omar",
+  "Othman",
+  "Amany",
+  "Samia",
+  "Anwar",
+];
+
+myAdmins.length = myAdmins.indexOf("Stop");
+document.write(`<div>We Have ${myAdmins.length} Admins</div>`);
+document.write(`<hr/>`);
+let count = 1;
+
+adminLoop: for (let i = 0; i < myAdmins.length; i++, count = 1) {
+  document.write(`The Admin For Team ${i + 1} Is ${myAdmins[i]}`);
+  document.write("<h1>Teem Members:</h1>");
+  employeesLoop: for (let j = 0; j < myEmployees.length; j++) {
+    if (myEmployees[j][0] === myAdmins[i][0])
+      document.write(`<p>- ${count++} ${myEmployees[j]}</p>`);
   }
+  document.write(`<hr/>`);
 }
