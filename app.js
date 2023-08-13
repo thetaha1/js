60c0776ff90bbcb8c4e878e703173aa040379a1c
thetaha1 <millionertaha@gmail.com>
2023-08-13 18:25:47 +0300

    Ass 11.1

--- app.js
+++ app.js
@@ -1,28 +1,14 @@
-// Loop Challenge
+let friends = ["Ahmed", "Sayed", "Ali", 1, 2, "Mahmoud", "Amany"];
+let index = 0;
+let counter = 0;
 
-let myAdmins = ["Ahmed", "Osama", "Sayed", "Stop", "Samera"];
-let myEmployees = [
-  "Amgad",
-  "Samah",
-  "Ameer",
-  "Omar",
-  "Othman",
-  "Amany",
-  "Samia",
-  "Anwar",
-];
+// No numbers , No words Start with letter 'A'
+// Output
+// "1 => Sayed"
+// "2 => Mahmoud"
 
-myAdmins.length = myAdmins.indexOf("Stop");
-document.write(`<div>We Have ${myAdmins.length} Admins</div>`);
-document.write(`<hr/>`);
-let count = 1;
-
-adminLoop: for (let i = 0; i < myAdmins.length; i++, count = 1) {
-  document.write(`The Admin For Team ${i + 1} Is ${myAdmins[i]}`);
-  document.write("<h1>Teem Members:</h1>");
-  employeesLoop: for (let j = 0; j < myEmployees.length; j++) {
-    if (myEmployees[j][0] === myAdmins[i][0])
-      document.write(`<p>- ${count++} ${myEmployees[j]}</p>`);
-  }
-  document.write(`<hr/>`);
+while (index < friends.length) {
+  if (typeof friends[index] !== "number" && friends[index][0] !== "A")
+    console.log(`${++counter} => ${friends[index]}`);
+  index++;
 }
