0770ea0450b709e5adfe20096011e9dfcee6deec
thetaha1 <millionertaha@gmail.com>
2023-08-14 12:17:07 +0300

    Ass 12.2

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
-function sayHello(theName, theGender) {
-  let nickآame = "";
-
-  if (theGender === "Male") {
-    nickآame = "Mr ";
-  } else if (theGender === "Female") {
-    nickآame = "Miss ";
+function calculate(firstNum, secondNum, operation) {
+  if (typeof firstNum === "undefined") {
+    console.log("NO NUMBERS");
+    return 0;
+  } else if (typeof secondNum === "undefined") {
+    console.log("Second Number Not Found");
+    return 0;
   }
-  console.log(`Hello ${nickآame}${theName}`);
+
+  if (operation === "add" || "undefined") {
+    console.log(`Addetion : ${firstNum + secondNum}`);
+  } else if (operation === "subtract") {
+    console.log(`Subtract : ${firstNum - secondNum}`);
+  } else if (operation === "multiply") {
+    console.log(`Multiply : ${firstNum * secondNum}`);
+  }
 }
 
 // Output
-sayHello("Osama", "Male"); // "Hello Mr Osama"
-sayHello("Eman", "Female"); // "Hello Miss Eman"
-sayHello("Sameh"); // "Hello Sameh"
+calculate(); // NO NUMBERS
+calculate(20); // Second Number Not Found
+calculate(20, 30); // 50
+calculate(20, 30, "add"); // 50
+calculate(20, 30, "subtract"); // -10
+calculate(20, 30, "multiply"); // 600
