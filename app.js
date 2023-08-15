a14ac34de335fde1c1b39f67dd179eb7b5dd0204
thetaha1 <millionertaha@gmail.com>
2023-08-15 11:50:59 +0300

    Ass 12.4

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
-let sal;
-let stt;
-let checker;
+let specialMix = (...data) => {
+  let result = 0;
+  let calcIfMixType = 0;
+  let check = () => {
+    for (let i = 0; i < data.length; i++) {
+      calcIfMixType = () => {
+        if (isNaN(parseInt(data[i])) === false) result += parseInt(data[i]);
+      };
+      calcIfMixType();
+    }
+  };
+  check();
+  if (result === 0) {
+    return "All Is Strings";
+  }
+  return result;
+};
 
-checker = (zName) =>
-  (stt = (status) =>
-    (sal = (salary) =>
-      status === "Available"
-        ? `${zName}, My Salary Is ${salary}`
-        : `Iam Not Avaialble`));
-
-console.log(checker("Osama")("Available")(4000)); // Osama, My Salary Is 4000
-console.log(checker("Ahmed")("Not Available")()); // Iam Not Avaialble
+console.log(specialMix(10, 20, 30)); // 60
+console.log(specialMix("10Test", "Testing", "20Cool")); // 30
+console.log(specialMix("Testing", "10Testing", "40Cool")); // 50
+console.log(specialMix("Test", "Cool", "Test")); // All Is Strings
