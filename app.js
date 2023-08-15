1c89df0f0cb88bb31e2cde67585a7a26bf3c3590
thetaha1 <millionertaha@gmail.com>
2023-08-15 06:23:43 +0300

    Training about function

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-// Function Arrow Challenges
-
-let names = (...na) => `String [${na.join("], [")}] => Done !`;
-
-console.log(names("Osama", "Mohamed", "Ali", "Ibrahim"));
-
-/* ================================= */
-
-let calc = (one, two, ...nums) => -one - two + nums[0] + nums[1];
-
-console.log(calc(10, 20, 50, 60)); // 80
+function taha(name, car) {
+  function abady() {
+    return `Your friend Abady car is ${car} Mr ${name}`;
+  }
+  function ret() {
+    return abady();
+  }
+  return ret();
+}
+console.log(taha("Taha", "Fourd"));
