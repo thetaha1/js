f6cce72e0710af9d6c0f15b11652739cbc420b82
thetaha1 <millionertaha@gmail.com>
2023-08-15 07:13:08 +0300

    Ass 13.2

--- app.js
+++ app.js
@@ -1,24 +1,7 @@
-function getDetails(zName, zAge, zCountry) {
-  function namePattern() {
-    let newName = zName.split(" ");
-    return `Hello ${newName[0]} ${newName[1][0]}.`;
-  }
-  function ageWithMessage() {
-    let currentAge = zAge.split(" ");
-    return `Your Age Is ${currentAge[0]}`;
-  }
-  function countryTwoLetters() {
-    let markOfCountry = zCountry.split("");
-    return `You Live In ${markOfCountry[0].toUpperCase()}${markOfCountry[1].toUpperCase()}`;
-  }
-  function fullDetails() {
-    return `${namePattern()}, ${ageWithMessage()}, ${countryTwoLetters()}`;
-  }
-  return fullDetails();
-}
+let itsMe = () => `Iam A Normal Function`;
 
-console.log(getDetails("Osama Mohamed", "38 Is My Age", "Egypt"));
-// Hello Osama M., Your Age Is 38, You Live In EG
+console.log(itsMe()); // Iam A Normal Function
 
-console.log(getDetails("Ahmed ali", "32 Is The Age", "Syria"));
-// Hello Ahmed A., Your Age Is 32, You Live In SY
+let urlCreate = (protocol, web, tld) => `${protocol}://www.${web}.${tld}`;
+
+console.log(urlCreate("https", "elzero", "org")); // https://www.elzero.org
