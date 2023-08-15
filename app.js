824323af17b581ebda174d12236f0395a8f0021c
thetaha1 <millionertaha@gmail.com>
2023-08-15 06:35:20 +0300

    Ass 13.1

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
-function taha(name, car) {
-  function abady() {
-    return `Your friend Abady car is ${car} Mr ${name}`;
+function getDetails(zName, zAge, zCountry) {
+  function namePattern() {
+    let newName = zName.split(" ");
+    return `Hello ${newName[0]} ${newName[1][0]}.`;
   }
-  function ret() {
-    return abady();
+  function ageWithMessage() {
+    let currentAge = zAge.split(" ");
+    return `Your Age Is ${currentAge[0]}`;
   }
-  return ret();
+  function countryTwoLetters() {
+    let markOfCountry = zCountry.split("");
+    return `You Live In ${markOfCountry[0].toUpperCase()}${markOfCountry[1].toUpperCase()}`;
+  }
+  function fullDetails() {
+    return `${namePattern()}, ${ageWithMessage()}, ${countryTwoLetters()}`;
+  }
+  return fullDetails();
 }
-console.log(taha("Taha", "Fourd"));
+
+console.log(getDetails("Osama Mohamed", "38 Is My Age", "Egypt"));
+// Hello Osama M., Your Age Is 38, You Live In EG
+
+console.log(getDetails("Ahmed ali", "32 Is The Age", "Syria"));
+// Hello Ahmed A., Your Age Is 32, You Live In SY
