de27cb2a5c5cbd8099037c7fa9155bb956f1831a
thetaha1 <millionertaha@gmail.com>
2023-08-20 07:29:08 +0300

    Ass 14 Challenge

---
+++ app.js
@@ -0,0 +1,32 @@
+/*
+  Higher Order Functions Challenges
+
+  You Can Use
+  - ,
+  - _
+  - Space
+  - True => 1 => One Time Only In The Code
+
+  You Cannot Use
+  - Numbers
+  - Letters
+
+  - You Must Use [Filter + Map + Reduce + Your Knowledge]
+  - Order Is Not Important
+  - All In One Chain
+
+*/
+
+let myString = "1,2,3,EE,l,z,e,r,o,_,W,e,b,_,S,c,h,o,o,l,2,0,Z";
+
+let solution = myString
+  .split(",")
+  .filter((el) => isNaN(el))
+  .map((el) => el[0])
+  .reduce((acc, cur) => acc + cur, "")
+  .toLowerCase()
+  .split("_")
+  .map((el) => el[0].toUpperCase() + el.slice(1))
+  .reduce((acc, cur) => acc + " " + cur);
+
+console.log(solution); // Elzero Web School
