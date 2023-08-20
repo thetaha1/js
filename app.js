be54eeeb975f88df404c9cf1aa1f1b907e3b18c5
thetaha1 <millionertaha@gmail.com>
2023-08-20 09:40:37 +0300

    Ass 14.2

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-let mix = [1, 2, 3, "E", 4, "l", "z", "e", "r", 5, "o"];
+let myString = "EElllzzzzzzzeroo";
 
-let letterWithoutNumbers = mix
-  .map((el) => (isNaN(el) ? el : ""))
-  .reduce((acc, cur) => acc + cur);
+let uniqueLetters = myString
+  .split('')
+  .filter((char, index, arr) => arr.indexOf(char) === index)
+  .reduce((cur, arr) => cur + arr);
 
-console.log(letterWithoutNumbers); // Elzero
+console.log(uniqueLetters); // Output: Elzero
