660dce9346a636c98c51e24ab3917962de8ea28e
thetaha1 <millionertaha@gmail.com>
2023-08-20 07:30:07 +0300

    Where

--- app.js
+++ app.js
@@ -21,12 +21,12 @@ let myString = "1,2,3,EE,l,z,e,r,o,_,W,e,b,_,S,c,h,o,o,l,2,0,Z";
 
 let solution = myString
   .split(",")
-  .filter((el) => isNaN(el))
+  .filter((el) => isNaN(el) && el !== "Z")
   .map((el) => el[0])
   .reduce((acc, cur) => acc + cur, "")
   .toLowerCase()
   .split("_")
-  .map((el) => el[0].toUpperCase() + el.slice(1))
+  .map(el => el[0].toUpperCase() + el.slice(1))
   .reduce((acc, cur) => acc + " " + cur);
 
 console.log(solution); // Elzero Web School
