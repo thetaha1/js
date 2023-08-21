73df5fbc37c0d83489ebcbad5c968faf3c5f0521
thetaha1 <millionertaha@gmail.com>
2023-08-21 15:19:42 +0300

    return to this

--- app.js
+++ app.js
@@ -1,25 +1,33 @@
-let a = 1;
-
-let threeNums = {
-  b: 2,
-  c: 3,
-  d: 4,
-};
-
-let twoNums = {
-  e: 5,
-  f: 6,
-};
-
-let finalObject = Object.assign({ a }, threeNums, twoNums);
-
-console.log(finalObject);
-
 /*
-  a: 1
-  b: 2
-  c: 3
-  d: 4
-  e: 5
-  f: 6
+  Function - Random Argument Challenge
+  ====================================
+  Create Function showDetails
+  Function Accept 3 Parameters [a, b, c]
+  Data Types For Info Is
+  - String => Name
+  - Number => Age
+  - Boolean => Status
+  Argument Is Random
+  Data Is Not Sorted Output Depend On Data Types
+  - Use Ternary Conditional Operator
 */
+
+function showDetails(a, b, c) {
+  let cont = [a, b, c];
+  return `Hello ${
+    cont.find((el) => typeof el === "string")
+  }, Your Age Is ${
+    cont.find((el) => typeof el === "number")
+  }, You Are ${
+    cont.find((el) => typeof el === "string")
+  } For Hire`;
+}
+
+showDetails("Osama", 38, true); // "Hello Osama, Your Age Is 38, You Are Available For Hire"
+showDetails(38, "Osama", true); // "Hello Osama, Your Age Is 38, You Are Available For Hire"
+showDetails(true, 38, "Osama"); // "Hello Osama, Your Age Is 38, You Are Available For Hire"
+showDetails(false, "Osama", 38); // "Hello Osama, Your Age Is 38, You Are Not Available For Hire"
+
+// let change = [false, "Osama", 38].find((el) => typeof el === "string");
+
+// console.log(change);
