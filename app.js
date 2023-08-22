0f3ecc0cb395ec361b007394c8bd956988121d54
thetaha1 <millionertaha@gmail.com>
2023-08-22 15:11:28 +0300

    Ass 15.1

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
 /* <div id="elzero" class="element" name="js">JavaScript</div> */
 
-let elementByTagName = document.getElementsByTagName("div");
-let elementByClassName = document.getElementsByClassName("element");
-let elementById = document.getElementById("elzero");
-let elementByName = document.getElementsByName("js");
-let elByQuselTagName = document.querySelector("div");
-let elByQuselId = document.querySelector("#elzero");
-let elByQuselClassName = document.querySelector(".element");
-let elByQuselName = document.querySelector("[name= 'js']");
-let elByQuselAllTagName = document.querySelectorAll("div");
-let elByQuselAllId = document.querySelectorAll("#elzero");
-let elByQuselAllClassName = document.querySelectorAll(".element");
-let elByQuselAllName = document.querySelectorAll("[name= 'js']");
+let elementByTagName = document.getElementsByTagName("div"); // 1
+let elementByClassName = document.getElementsByClassName("element"); // 2
+let elementById = document.getElementById("elzero"); // 3
+let elementByName = document.getElementsByName("js"); // 4
+let elByQuselTagName = document.querySelector("div");  // 5
+let elByQuselId = document.querySelector("#elzero"); // 6
+let elByQuselClassName = document.querySelector(".element"); // 7
+let elByQuselName = document.querySelector("[name= 'js']"); // 8
+let elByQuselAllTagName = document.querySelectorAll("div"); // 9
+let elByQuselAllId = document.querySelectorAll("#elzero"); // 10
+let elByQuselAllClassName = document.querySelectorAll(".element"); // 11
+let elByQuselAllName = document.querySelectorAll("[name= 'js']"); // 12
+let elByFirstElementChild = document.body.firstElementChild; // 13
+let leByFirstChild = document.body.firstChild; // 14
+let elByChildien = document.body.children[0]; // 15
 
-console.log(el);
+console.log(elementByTagName);
+console.log(elementByClassName);
+console.log(elementById);
+console.log(elementByName);
+console.log(elByQuselTagName);
+console.log(elByQuselId);
+console.log(elByQuselClassName);
+console.log(elByQuselName);
+console.log(elByQuselAllTagName);
+console.log(elByQuselAllId);
+console.log(elByQuselAllClassName);
+console.log(elByQuselAllName);
+console.log(elByFirstElementChild);
+console.log(leByFirstChild);
+console.log(elByChildien);
